21a0d821cb3a226a462e8aa63c6baf5db8d48e26
Murat Yaşar <murat.yasar@comkom.de>
2026-07-02 00:12:28 +0200

    add time.cpp

---
+++ time.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// Variables
+string name = "Murat Yasar";
+int birth_year = 1982;
+
+int main()
+{
+  cout << name << " is born in " << birth_year;
+}
